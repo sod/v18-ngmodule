7a6e214c7e435fedea22f08ff56713d34852e6a0
Alexander von Weiss <alexander.weiss@rebuy.com>
2024-05-23 15:37:34 +0200

    add two shared modules and a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Shared1Module } from './shared1/shared1.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Shared1Module],
   template: `
     <h1>Welcome to {{title}}!</h1>
+    <app-foo />
 
     <router-outlet />
   `,
---
+++ src/app/shared1/foo/foo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  template: `
+    <p>
+      foo works!
+    </p>
+  `,
+  styles: ``
+})
+export class FooComponent {
+
+}
